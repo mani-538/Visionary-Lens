222642844301cc186b99120b4e39932ee0d29fe9
mani-538 <manikandan.aids.25@gmail.com>
2026-06-10 16:03:40 +0530

    Updated backend: auth routes, main, models, services

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,14 @@ from fastapi import FastAPI
 from backend.app.routes.home_routes import router as home_router
 from backend.app.routes.auth_routes import router as auth_router
 
+from backend.app.database.db_connection import engine, Base
+from backend.app.database import models
+
 app = FastAPI()
 
 app.include_router(home_router)
 app.include_router(auth_router)
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
--- backend/app/routes/auth_routes.py
+++ backend/app/routes/auth_routes.py
@@ -2,19 +2,64 @@ from fastapi import APIRouter
 from backend.app.schemas.user_schema import UserRegister
 from backend.app.schemas.user_schema import UserRegister, UserLogin
 
+from backend.app.services.auth_service import hash_password
+
+from backend.app.database.db_connection import SessionLocal
+from backend.app.database.models import User
+from backend.app.services.auth_service import hash_password
+
+from backend.app.services.auth_service import verify_password
+
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 
 @router.post("/register")
 def register(user: UserRegister):
+    db = SessionLocal()
+
+    hashed_pwd = hash_password(user.password)
+
+    new_user = User(
+        full_name=user.full_name,
+        email=user.email,
+        password_hash=hashed_pwd
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    db.close()
+
     return {
-        "message": "User registration endpoint working",
-        "user": user
+        "message": "User registered successfully",
+        "user": {
+            "id": new_user.id,
+            "full_name": new_user.full_name,
+            "email": new_user.email
+        }
     }
 
 @router.post("/login")
 def login(user: UserLogin):
+    db = SessionLocal()
+
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        db.close()
+        return {"message": "Invalid email or password"}
+
+    if not verify_password(user.password, db_user.password_hash):
+        db.close()
+        return {"message": "Invalid email or password"}
+
+    db.close()
+
     return {
-        "message": "Login endpoint working",
-        "email": user.email
+        "message": "Login successful",
+        "user": {
+            "id": db_user.id,
+            "email": db_user.email,
+            "full_name": db_user.full_name
+        }
     }
