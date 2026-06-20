9cfd17f8255afcf49e0507bfb7f6d4f772455cb9
mani-538 <manikandan.aids.25@gmail.com>
2026-06-20 12:20:43 +0530

    Connect Flutter app to Render backend

--- mobile/lib/core/constants.dart
+++ mobile/lib/core/constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
-  static const String baseUrl = "http://10.176.137.226:8000";
+  static const String baseUrl = "https://visionary-lens-backend.onrender.com";
   static const String visionPath = "/vision/analyze-image";
   static const String historyPath = "/vision/history";
   static const String loginPath = "/auth/login";
